09cd64ee59c1f840ed5e4dca2d9d290a138c6c45
Bruno Marques <brunomarques.web@gmail.com>
2019-01-06 11:58:56 -0200

    Primeiro commit - projeto criado

---
+++ src/main/java/br/com/brunomarques/springmongo/SpringbootMongodbApplication.java
@@ -0,0 +1,14 @@
+package br.com.brunomarques.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbApplication.class, args);
+	}
+
+}
+
