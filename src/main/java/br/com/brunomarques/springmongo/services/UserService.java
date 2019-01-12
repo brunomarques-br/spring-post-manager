abf20f2ff7a98e98e5b47a8bbe0c061c7c267dd6
Bruno Marques <brunomarques.web@gmail.com>
2019-01-12 00:20:57 -0200

    Inserção de usuário com POST e retorno do último registro cadastrado.

--- src/main/java/br/com/brunomarques/springmongo/services/UserService.java
+++ src/main/java/br/com/brunomarques/springmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.brunomarques.springmongo.services;
 
 import br.com.brunomarques.springmongo.domain.User;
+import br.com.brunomarques.springmongo.dto.UserDTO;
 import br.com.brunomarques.springmongo.repository.UserRepository;
 import br.com.brunomarques.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,17 @@ public class UserService {
         return repo.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
