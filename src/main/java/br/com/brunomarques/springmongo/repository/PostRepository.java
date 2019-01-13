f6ec840870c9f699a5fb4a10cd8180b28629780a
Bruno Marques <brunomarques.web@gmail.com>
2019-01-13 15:45:14 -0200

    Consulta simples @Query * Em PostRepository, foi criado a implementação alternativa da consulta * Em PostService, foi atualizado a chamada da consulta
    ** Query de referência: https://docs.mongodb.com/manual/reference/operator/query/regex/#op._S_regex

--- src/main/java/br/com/brunomarques/springmongo/repository/PostRepository.java
+++ src/main/java/br/com/brunomarques/springmongo/repository/PostRepository.java
@@ -2,12 +2,19 @@ package br.com.brunomarques.springmongo.repository;
 
 import br.com.brunomarques.springmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    /*
+    * Documentação de referência: https://docs.mongodb.com/manual/reference/operator/query/regex/#op._S_regex
+    * */
+    @Query("{ 'title' : { $regex: ?0 , $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
